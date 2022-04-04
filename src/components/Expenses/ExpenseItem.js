5ac33e03b9cf25961e8660118d79d774166a86e5
cslusher <slusher.cj@gmail.com>
2022-04-03 21:14:18 -0700

    Section 3: React Basics & Working With Components

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,20 @@
+import "./ExpenseItem.css";
+import ExpenseDate from "./ExpenseDate";
+import Card from "../UI/Card";
+
+const ExpenseItem = ({ title, amount, date }) => {
+  const expenseTitle = "Car Insurance";
+  const expenseAmount = 294.67;
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={date} />
+      <div className="expense-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
